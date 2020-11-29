3ce00339915c1e1e642788ac8541ce3b0c7b165f
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2020-11-29 04:56:07 -0500

    Fixed with a few important comments added

--- song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
+++ song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
@@ -90,6 +90,8 @@ public class SongDalImpl implements SongDal {
 
 	  try {
 	    
+	    // FIX INVALID INPUT CHECK HERE :( DON'T RMMR HOW DIVYAM DID IT and too sleepy
+	    
 	    db.getCollection("songs").updateOne(Filters.eq("_id", new ObjectId(songId)), Updates.set("songAmountFavourites", currentVal+increment));
 	    toReturn.setData(returnVal);
 	    toReturn.setdbQueryExecResult(DbQueryExecResult.QUERY_OK);
