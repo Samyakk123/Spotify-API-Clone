b772a63964012ad2f8c71e01835b184dd0a9bbd6
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2020-12-01 19:52:44 -0500

    Finished like-song implementation

--- profile-microservice/src/main/java/com/csc301/profilemicroservice/PlaylistDriverImpl.java
+++ profile-microservice/src/main/java/com/csc301/profilemicroservice/PlaylistDriverImpl.java
@@ -1,11 +1,14 @@
 package com.csc301.profilemicroservice;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
 import org.springframework.stereotype.Repository;
-
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.Call;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -42,6 +45,7 @@ public class PlaylistDriverImpl implements PlaylistDriver {
 	  
 	  
 	  toReturn = new DbQueryStatus("", DbQueryExecResult.QUERY_OK);
+	  ObjectMapper mapper = new ObjectMapper();
 	  HttpUrl.Builder urlBuilder = HttpUrl.parse("http://localhost:3001" + "/getSongTitleById/" + songId).newBuilder();
 	  
 //	  urlBuilder.addQueryParameter("songId", songId);
@@ -50,7 +54,6 @@ public class PlaylistDriverImpl implements PlaylistDriver {
 	  
 	  
 	  String url = urlBuilder.build().toString();
-	  System.out.println("important line here: " +  url);
 	  
 //	  RequestBody body = RequestBody.create(null, new byte[0]);
 
@@ -64,11 +67,49 @@ public class PlaylistDriverImpl implements PlaylistDriver {
 
 	    responseFromAddMs = call.execute();
 
+	    
+	    
 	    addServiceBody = responseFromAddMs.body().string();
-	    System.out.println(addServiceBody);
+	    
+	    
+	    mapper.readValue(addServiceBody, Map.class);
+	    
+	    
+	    String song = (String) mapper.readValue(addServiceBody, Map.class).get("data");
 
-	    toReturn.setdbQueryExecResult(DbQueryExecResult.QUERY_OK);
-	    toReturn.setData("OK");
+	    // Verify that it was returned
+	    if(song == null) {
+	      toReturn.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_GENERIC);
+	      return toReturn;
+	    }
+	    
+	      
+	      try (Session session = ProfileMicroserviceApplication.driver.session()) {
+	        try (Transaction trans = session.beginTransaction()) {
+	          System.out.println("hi"); 
+	             Map<String, Object> toInsert = new HashMap<String, Object>(); 
+	             toInsert.put("userName", userName);
+	             toInsert.put("plName", userName + "-favorites");
+	             toInsert.put("song", song);
+	             
+	             trans.run("\n MATCH (a:profile {userName:$userName})-[r:created]->(b:playlist {plName:$plName})\n"
+	                 + " MERGE (b)-[d:favorites]-(c:song {title:$song})", toInsert);
+	             trans.success();
+	             
+//	             MATCH (a:profile {userName: "test"})-[r:created]->(b:playlist {plName:"test-favorites"})
+//	             MERGE (b)-[d:created]-(c:song {title: "I should have came"})
+	             
+	        }
+	        session.close();
+	        return toReturn; 
+	        
+	      }    
+
+	    
+	    
+	    
+//	    toReturn.setdbQueryExecResult(DbQueryExecResult.QUERY_OK);
+//	    toReturn.setData("OK");
 	    
 	    
 	  }catch(Exception e) {
@@ -78,6 +119,9 @@ public class PlaylistDriverImpl implements PlaylistDriver {
 	  
 	  return toReturn;
 	}
+	
+
+	
 
 	@Override
 	public DbQueryStatus unlikeSong(String userName, String songId) {
